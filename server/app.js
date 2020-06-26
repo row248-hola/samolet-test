f6ae5492ab2076928091867a25d8d5cd36bc14ab
voit_egor <e.voit@busfor.com>
2020-06-26 19:17:21 +0300

    remove data mocks

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const proxy = require('http-proxy-middleware');
 
 app.use(express.static("public"));
+app.use('/opendata', proxy({ target: 'https://data.gov.ru/sites/default/files', changeOrigin: true /*, secure: true */ }));
 
 app.listen(3000, () => {
-  app.get("*", (req, res) => {
+  app.get(/\/(details)?/, (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"));
   });
 });
--- src/api.js
+++ src/api.js
@@ -1,12 +1,8 @@
-import data from './data';
+const BASE_URL = '/opendata/';
 
-// const BASE_URL = 'https://data.gov.ru/sites/default/files/opendata/';
-
-// @todo: need server proxy for fixing CORS issue
 const fetchData = () => {
-  return Promise.resolve(data);
-  // return fetch(`${BASE_URL}7705851331-stat_library/data-2016-11-10T00-00-00-structure-2016-09-12T00-00-00.json`)
-  //   .then(response => response.json());
+  return fetch(`${BASE_URL}7705851331-stat_library/data-2016-11-10T00-00-00-structure-2016-09-12T00-00-00.json`)
+    .then(response => response.json());
 };
 
 export {
